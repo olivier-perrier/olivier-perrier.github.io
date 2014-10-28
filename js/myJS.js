ce94cfee11c92309445de957c7ecbd5d1d4c5c3d
olivier-perrier <olivier.Perrier.j@gmail.com>
2014-10-28 15:06:32 +0100

    premier push CV 3 eme version

---
+++ js/myJS.js
@@ -0,0 +1,13 @@
+//$(function() {
+//
+//	$('#intro-text').hide().scroll().fadeIn(1000);
+//
+//	 $('#services-items').hide();
+//
+//	$('#services').mouseover(function() {
+//		 $('#services-items').show();
+//		$('#services-items').animate({
+//			top : '+=200'
+//		}, 'slow');
+//	});
+//});
